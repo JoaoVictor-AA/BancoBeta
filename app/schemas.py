806bd01e0c794e164ba70d25f7f2a2ac5a8d38e7
JoaoVictor-AA <joaovictorafonso1111@gmail.com>
2025-12-24 11:58:29 -0300

    schemas done

---
+++ app/schemas.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+from pydantic.types import conint
+
+class UserOut(BaseModel):
+    name: str
+    id: int
+
+
+class CreateUser(BaseModel):
+    name: str
+    password: str
+    email: str
+
+
+class MakeTransaction(BaseModel):
+    sender_id: int
+    receiver_id: int
+    valor: float
+
+
+class TransactionOut(BaseModel):
+    sender_id: int
+    receiver_id: int
+    valor: float
+    uuid: UUID
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
